eed833859ee72952d88e5989f214f52204d185e1
Kevin Jeongho Yang <jeonghoyang@Kevins-MacBookPro.local>
2022-01-04 01:28:52 +0900

    [pages/index.js] separate each sections to differenct components

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import { makeStyles } from "@mui/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
@@ -6,7 +7,11 @@ import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import Link from "@mui/material/Link";
+import NavRow from "../components/NavRow";
+import About from "../components/About";
+import Experiences from "../components/Experiences";
+import Skills from "../components/Skills";
+import Projects from "../components/Projects";
 
 const useStyles = makeStyles({
   container: {
@@ -15,75 +20,52 @@ const useStyles = makeStyles({
     maxWidth: "700px",
   },
   typography: {
-    paddingTop: "20px",
-    color: "#707070",
-    fontSize: "18px",
+    // paddingTop: "10px",
+    fontWeight: "bold",
+    fontSize: "28px",
   },
   divider: {
-    paddingTop: "50px",
+    paddingTop: "40px",
   },
   nav: {
     paddingTop: "20px",
-    color: "#707070",
-  },
-  navText: {
-    fontSize: "10px",
   },
 });
 
-function NavRow() {
-  return (
-    <React.Fragment>
-      <Grid item xs="auto" xs={6} sm={3}>
-        <Link underline="none" color="#707070" href="#">
-          <Typography fontSize="13px">👤 ABOUT</Typography>
-        </Link>
-      </Grid>
-      <Grid item xs="auto" xs={6} sm={3}>
-        <Link underline="none" color="#707070" href="#">
-          <Typography fontSize="13px">💼 EXPERIENCES</Typography>
-        </Link>
-      </Grid>
-      <Grid item xs="auto" xs={6} sm={3}>
-        <Link underline="none" color="#707070" href="#">
-          <Typography fontSize="13px">💻 SKILLS</Typography>
-        </Link>
-      </Grid>
-      <Grid item xs="auto" xs={6} sm={3}>
-        <Link underline="none" color="#707070" href="#">
-          <Typography fontSize="13px">📋 PROJECTS</Typography>
-        </Link>
-      </Grid>
-    </React.Fragment>
-  );
-}
-
 export default function Home() {
   const classes = useStyles();
-
   return (
     <React.Fragment>
       <CssBaseline />
       <Container className={classes.container} maxWidth="sm">
+        {/* Main page */}
         <Typography variant="h4" style={{ paddingBottom: "10px" }}>
           🧑‍💻
         </Typography>
-        <Typography variant="h4">
-          <strong>Hi!</strong>
+        <Typography className={classes.typography} variant="h4">
+          I am Kevin, a software engineer, <br /> based in Seoul, South Korea.
         </Typography>
-        <Typography className={classes.typography} variant="h6">
-          I am <strong>Kevin</strong>, software engineer,
-          <br />
-          currently studying computer science in Ohio, United States.
+        <Typography
+          variant="h6"
+          style={{ fontSize: "15px", paddingTop: "20px" }}
+        >
+          I like to build a server structure using back-end engineering, and
+          little bit of UI front-end programming.
         </Typography>
         <Divider className={classes.divider} />
         <Box sx={{ flexGrow: 1 }}>
-          <Grid className={classes.nav} container spacing={3}>
-            <Grid container item sx={1} spacing={3}>
+          <Grid className={classes.nav} container spacing={1}>
+            <Grid container item sx={3} spacing={4}>
               <NavRow />
             </Grid>
           </Grid>
         </Box>
+
+        {/* Body */}
+        <About />
+        <Experiences />
+        <Skills />
+        <Projects />
       </Container>
     </React.Fragment>
   );

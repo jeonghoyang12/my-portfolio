d5ea1f781328e36cad60919efa8a249293d1d4ff
Kevin Jeongho Yang <jeonghoyang@Kevins-MacBookPro.local>
2022-01-26 01:12:40 +0900

    [components/Experience] edit styling

--- components/Experiences.js
+++ components/Experiences.js
@@ -55,7 +55,12 @@ export default function Experiences() {
             </Grid>
             {experience.description.map((des) => {
               return (
-                <Typography key={des} sx={{ fontSize: "14px", pt: "10px" }}>
+                <Typography
+                  key={des}
+                  variant="body2"
+                  component="h1"
+                  className={classes.bodyText}
+                >
                   <li>{des}</li>
                 </Typography>
               );
